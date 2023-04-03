2bf69ef15916d89b736d41a0dadf89eae4704636
sedub01 <dubenkov.semen@mail.ru>
2023-04-03 16:50:45 +0300

    Изменена цветовая палитра Теперь если параметр colorness <0, то цвет становится не чернее, а насыщеннее

--- src/main/java/root/utils/Global.java
+++ src/main/java/root/utils/Global.java
@@ -4,24 +4,28 @@ import javafx.scene.paint.Color;
 
 //Хранилище глобальных функций
 public class Global {
-    public static String getCSSThemeColor(double whiteness, String key){
-        final Color tColor = Constants.THEME_COLOR;
+    public static String getCSSThemeColor(double colorness, String key){
+        Color tColor = Constants.THEME_COLOR;
+        if (colorness < 0) {
+            tColor = tColor.saturate();
+        }
+
         final int red = (int)(tColor.getRed()*255);
         final int green = (int)(tColor.getGreen()*255);
         final int blue = (int)(tColor.getBlue()*255);
-        int dRed = (int)((255 - red)*whiteness);
-        int dGreen = (int)((255 - green)*whiteness);
-        int dBlue = (int)((255 - blue)*whiteness);
-        if (whiteness < 0 || whiteness > 1){
-            Logger.log("Параметр whiteness должен быть в пределах [0; 1]");
+        int dRed = (int)((255 - red)*colorness);
+        int dGreen = (int)((255 - green)*colorness);
+        int dBlue = (int)((255 - blue)*colorness);
+        if (colorness < -1 || colorness > 1){
+            Logger.log("Параметр colorness должен быть в пределах [-1; 1]");
             dRed = dGreen = dBlue = 0;
         }
 
         final String pattern = (key.equals("color"))? Constants.BG_THEME_COLOR_PATTERN: Constants.BG_THEME_PATTERN;
         return String.format(pattern, red + dRed, green + dGreen, blue + dBlue);
     }
-    public static String getCSSThemeColor(double whiteness){
-        return getCSSThemeColor(whiteness, "color");
+    public static String getCSSThemeColor(double colorness){
+        return getCSSThemeColor(colorness, "color");
     }
     public static String getCSSThemeColor(){
         return getCSSThemeColor(0);
